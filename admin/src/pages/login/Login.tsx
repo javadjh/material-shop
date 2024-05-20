54fa71b6fb3d5d03dd4336c9dc7f9210dd0fb8e9
javad <48103478+javadjh@users.noreply.github.com>
2024-05-20 19:30:19 +0330

    fix

--- admin/src/pages/login/Login.tsx
+++ admin/src/pages/login/Login.tsx
@@ -1,14 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import { loginService } from "../../service/user.service";
 import { ILoginUser } from "../../types/user.type";
 import LoginBoxComponent from "./LoginBox.c";
 import { LoginContainerStyled } from "./login.style";
 
 const Login = () => {
+  const navigate = useNavigate();
   const login = async (user: ILoginUser) => {
     const {
       data: { data: token },
     } = await loginService(user);
     localStorage.setItem("token", token);
+
+    navigate("/");
   };
   return (
     <LoginContainerStyled>
